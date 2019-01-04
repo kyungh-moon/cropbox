691de94b77e6b7e5d0f85f42d3e169306469db07
Kyungdahm Yun <tomyun@gmail.com>
2019-01-03 16:36:48 -0800

    Setup initial repo

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name="cropbox",
+    version="0.1",
+    descrption='Modern crop modeling framework',
+    long_description='',
+    url='https://github.com/tomyun/cropbox',
+    author='Kyungdahm Yun',
+    author_email='tomyun@gmail.com',
+    #license='MIT',
+    packages=find_packages(),
+)
