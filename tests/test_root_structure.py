915c30c6aee7285ab4a0360e3b65703acd4a8bc2
Kyungdahm Yun <tomyun@gmail.com>
2019-02-20 00:02:48 -0800

    Clean up root test

--- tests/test_root_structure.py
+++ tests/test_root_structure.py
@@ -25,35 +25,21 @@ import numpy as np
 
 def test_root_structure(instance):
     class R(System):
-        @accumulate
-        def age(self):
-            return 1.0
-
         @derive
         def elongation_rate(self):
-            # if self.age > 10:
-            #     return 0.2
-            #return 1.0
             return random.gauss(1.0, 0.2)
 
         @static
         def branching_angle(self):
-            #return 30
-            return random.gauss(30, 10)
+            return random.gauss(20, 10)
 
         @parameter
         def branching_interval(self):
-            return 5.0
+            return 3.0
 
         @parameter
         def branching_chance(self):
-            #return 1
             return 0.5
-
-        @parameter
-        def branching_zone(self):
-            #return 1.0
-            return 0
 
         @derive
         def branched_length(self):
@@ -75,44 +61,43 @@ def test_root_structure(instance):
             if l - ll > self.branching_interval:
                 if random.random() <= self.branching_chance:
                     print(f'branch at l = {l}')
-                    self.branch(R, **{'branched_length': l - self.branching_zone})
+                    self.branch(R, **{'branched_length': l})
                     return l
             return ll
 
         def render(self):
             s = trimesh.scene.scene.Scene()
-            #TODO: make System's own walker method
+            #TODO: make System's own walker method?
             def visit(r, pn=None):
-                if r.length == 0:
+                l = r.length
+                if l == 0:
                     return
-                m = trimesh.creation.cylinder(radius=r.diameter, height=r.length)
+                m = trimesh.creation.cylinder(radius=r.diameter, height=l)
                 if pn is None:
                     m.visual.face_colors = (255, 0, 0, 255)
-                #m.apply_translation((0, 0, -r.length/2 + r.branching_zone))
-                m.apply_translation((0, 0, -r.length/2))
-                y = (random.random() > 0.5) * 2 - 1
-                R = trimesh.transformations.rotation_matrix(angle=np.radians(r.branching_angle), direction=(0, y, 0))
-                #T = trimesh.transformations.translation_matrix((0, 0, r.length - r.branching_zone))
-                T = trimesh.transformations.translation_matrix((0, 0, r.length))
-                if pn is None:
-                    T2 = trimesh.transformations.translation_matrix((0, 0, 0))
-                else:
-                    T2 = trimesh.transformations.translation_matrix((0, 0, -r.parent.length + r.branched_length))
-                M = trimesh.transformations.concatenate_matrices(T2, R, T)
-
+                # put segment end at origin
+                m.apply_translation((0, 0, l/2))
+                # put root end at parent's tip
+                T1 = trimesh.transformations.translation_matrix((0, 0, -l))
+                # rotate root segment along random axis (x: random, y: left or right)
+                angle = np.radians(r.branching_angle)
+                direction = (random.random(), (random.random() > 0.5) * 2 - 1, 0)
+                R = trimesh.transformations.rotation_matrix(angle, direction)
+                # put root segment at parent's branching point
+                z = 0 if pn is None else r.parent.length - r.branched_length
+                T2 = trimesh.transformations.translation_matrix((0, 0, z))
+                M = trimesh.transformations.concatenate_matrices(T2, R, T1)
+                # add root segment
                 n = s.add_geometry(m, parent_node_name=pn, transform=M)
-                for cr in r.children:
-                    visit(cr, n)
+                # visit recursively
+                [visit(cr, n) for cr in r.children]
             visit(self)
             s.show()
             return s
 
     r = instance(R)
     c = r.context
-    T = range(50)
-    H = []
-    P = []
-    for t in T:
-        c.update()
+    T = range(40)
+    [c.update() for t in T]
     r.render()
     breakpoint()
