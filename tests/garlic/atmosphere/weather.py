fa37d6896b54139c58aa3e5439c2168203811a79
Kyungdahm Yun <tomyun@gmail.com>
2019-05-12 18:37:09 -0700

    WIP (plant)

---
+++ tests/garlic/atmosphere/weather.py
@@ -0,0 +1,89 @@
+from cropbox.system import System
+from cropbox.statevar import derive, drive, parameter, system
+
+from numpy import exp
+import pandas as pd
+
+class VaporPressure(System):
+    # Campbell and Norman (1998), p 41 Saturation vapor pressure in kPa
+    a = parameter(0.611) # kPa
+    b = parameter(17.502) # C
+    c = parameter(240.97) # C
+
+    @derive(alias='es')
+    def saturation(self, T, *, a, b, c):
+        return a*exp((b*T)/(c+T))
+
+    @derive(alias='ea')
+    def ambient(self, T, RH):
+        return self.es(T) * RH
+
+    @derive(alias='vpd')
+    def deficit(self, T, RH):
+        return self.es(T) * (1 - RH)
+
+    @derive(alias='rh')
+    def relative_humidity(self, T, VPD):
+        return 1 - VPD / self.es(T)
+
+    # slope of the sat vapor pressure curve: first order derivative of Es with respect to T
+    @derive(alias='cs')
+    def curve_slope(self, T, P, *, b, c):
+        return self.es(T) * (b*c)/(c+T)**2 / P
+
+#TODO: use improved @drive
+#TODO: implement @unit
+class Weather(System):
+    @system(alias='vp')
+    def vapor_pressure(self):
+        return VaporPressure
+
+    @parameter
+    def filename(self):
+        return ''
+
+    @constant(alias='df')
+    def dataframe(self):
+        return pd.read_csv(self.filename)
+
+    @derive
+    def key(self):
+        return self.context.time
+
+    @derive
+    def store(self):
+        return self.df.loc[self.key]
+
+    @drive
+    def PFD(self): #return 1500 # umol m-2 s-1
+        return self.store
+
+    @drive
+    def CO2(self): #return 400 # ppm
+        return self.store
+
+    @drive
+    def RH(self): #return 0.6 # 0~1
+        return self.store
+
+    @drive
+    def T_air(self): #return 25 # C
+        return self.store
+
+    @drive
+    def wind(self): #return 2.0 # meters s-1
+        return self.store
+
+    @drive
+    def P_air(self): #return 100 # kPa
+        return self.store
+
+    @derive
+    def VPD(self, T_air, RH): return self.vp.deficit(T_air, RH)
+
+    @derive
+    def VPD_slope(self, T_air, P_air): return self.vp.curve_slope(T_air, P_air)
+
+    def __str__(self):
+        w = self
+        return f'PFD = {w.PFD}, CO2 = {w.CO2}, RH = {w.RH}, T_air = {w.T_air}, wind = {w.wind}, P_air = {w.P_air}'
---
+++ tests/garlic/physiology/plant.py
@@ -0,0 +1,49 @@
+from cropbox.system import System
+from cropbox.statevar import derive, drive, parameter, produce, system
+
+class Plant(System):
+    weather = system(Weather)
+    soil = system(Soil)
+
+    phenology = system(Phenology, alias='pheno', plant='self')
+    photosynthesis = system(Photosynthesis, plant='self')
+
+    mass = system(Mass, plant='self')
+    area = system(Area, plant='self')
+    count = system(Count, plant='self')
+    ratio = system(Ratio, plant='self')
+    carbon = system(Carbon, plant='self')
+    nitrogen = system(Nitrogen, plant='self')
+    water = system(Water, plant='self')
+
+    #TODO pass PRIMORDIA as initial_leaves
+    primordia = parameter(5)
+
+    bulb = system(None)
+    scape = system(None)
+    root = system(None)
+    nodal_units = system([])
+
+    @produce(target='nodal_units')
+    def initiate_primordia(self):
+        if len(self.nodal_units) == 0:
+            return [(NodalUnit, {'rank': i+1}) for i in range(self.primordia)]
+
+    @produce(target='root')
+    def initiate_root(self):
+        if not self.root:
+            if self.pheno.emerging:
+                #TODO import_carbohydrate(self.soil.total_root_weight)
+                return (Root, {})
+
+    @produce(target='nodal_units')
+    def initiate_leaves(self):
+        if not (self.pheno.germinating or self.pheno.dead):
+            def f(i):
+                try:
+                    self.nodal_units[i]
+                except IndexError:
+                    return (NodalUnit, {'rank': i+1})
+                else:
+                    return None
+            return [f(i) for i in range(self.pheno.leaves_initiated)]
